515b25d629ff76c1d8fac7c3294e4575b1fc529e
Shaik Rehman <shaikrehman78609@gmail.com>
2025-06-04 17:38:43 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -18,10 +18,39 @@
 
 
 
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token");
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default api;
+
+
+
+
+
+
+
+
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 api.interceptors.request.use(
@@ -38,3 +67,4 @@ api.interceptors.request.use(
 );
 
 export default api;
+
